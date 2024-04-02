a0a1afe9032c746bc3cee39c182178af856c0a49
Philip Haggard <philip.r.haggard@gmail.com>
2024-04-02 14:16:10 -0500

    Re-style program.cs file

--- API/Program.cs
+++ API/Program.cs
@@ -7,22 +7,28 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 
-var builder = WebApplication.CreateBuilder(args);
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+        // Add services to the container.
 
-builder.Services.AddControllers();
-builder.Services.AddApplicationServices(builder.Configuration);
-builder.Services.AddIdentityServices(builder.Configuration);
+        builder.Services.AddControllers();
+        builder.Services.AddApplicationServices(builder.Configuration);
+        builder.Services.AddIdentityServices(builder.Configuration);
 
-var app = builder.Build();
+        var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
+        // Configure the HTTP request pipeline.
+        app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
 
-app.UseAuthentication(); // do you have a valid token?
-app.UseAuthorization(); // you have a valid token, now what are you allowed to do?
+        app.UseAuthentication(); // do you have a valid token?
+        app.UseAuthorization(); // you have a valid token, now what are you allowed to do?
 
-app.MapControllers();
+        app.MapControllers();
 
-app.Run();
+        app.Run();
+    }
+}
